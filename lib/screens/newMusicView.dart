523c37a96ae1f3d959b208dd6460a85ef01e7b46
Suchy <marcinsuchora@gmail.com>
2019-09-24 23:55:19 +0200

    Probably new approach?

--- lib/screens/newMusicView.dart
+++ lib/screens/newMusicView.dart
@@ -13,14 +13,12 @@ class _newMusicView extends State<newMusicView>{
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Fox'),
       home: Scaffold(
-          body:Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
+          body:Container(
                     color: Color(MyColors.teal_green),
                     height: MediaQuery.of(context).size.height,
                     child: Padding(padding: EdgeInsets.fromLTRB(0, 30, 0, 0),child:
                     Column(children: <Widget>[
+                      SizedBox(height: 16,),
                       Row(mainAxisAlignment:MainAxisAlignment.spaceEvenly,children: <Widget>[
                         Icon(Icons.music_note,size: 32,color: Color(MyColors.begonia),),
                         Icon(Icons.library_music,size: 32,color: Color(MyColors.begonia)),
@@ -33,17 +31,23 @@ class _newMusicView extends State<newMusicView>{
                         width: MediaQuery.of(context).size.width/1.5,
                         decoration: BoxDecoration(color: Color(MyColors.begonia).withOpacity(0.7),borderRadius: BorderRadius.circular(8)),
                         child: TextField(textAlignVertical: TextAlignVertical.center,showCursor: false,decoration: InputDecoration(border: InputBorder.none,contentPadding: EdgeInsets.all(5),alignLabelWithHint: true,hintText: "Looking for an album?"),),
-                      )
+                      ),
+                      SizedBox(height: 16,),
+                      
                     ],),)
                 ),
 
-              ),
-              Container(
-                width: 70.0,
-                color: Color(MyColors.aquamarine),
-              )
-            ],
-          )),
+      bottomNavigationBar: BottomAppBar(child:
+      Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,children: <Widget>[
+        IconButton(icon: Icon(Icons.arrow_back),),
+        IconButton(icon: Icon(Icons.shuffle),),
+        IconButton(icon: Icon(Icons.play_arrow),),
+        IconButton(icon: Icon(Icons.loop),),
+        IconButton(icon: Icon(Icons.arrow_forward),),
+      ],),
+      )
+      ),
+
     );
   }
 }
